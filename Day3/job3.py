05d482aaf31ea9ee2ee725b99fc3ebcc926be676
elodie-boweren <elodie.boweren@laplateforme.io>
2025-02-09 22:22:23 +0100

    docs: add job 3 day 3

--- Day3/job3.py
+++ Day3/job3.py
@@ -1,25 +1,60 @@
-class Chores:
-    def __init__(self):
-        self.title = ""
-        self.description = ""
-        self.status_to_do = True
-        self.status_done = False
+class Chore:
+    def __init__(self, title, description, status):
+        self.title = title
+        self.description = description
+        self.status = status
+    
+    def __str__(self):
+        return(f"""
+    {self.title} = {self.description} - Status = {self.status}
+              """)
 
 class List_of_chores:
-    def __init__(self, chore : Chores, status_done, status_to_do):
+    def __init__(self):
         self.chores = []
 
-    def add_chore(self, chore):
-        self.chores += chore
+    def add_chore(self, chore : Chore):
+        self.chores.append(chore)
 
-    def remove_chore(self, chore):
-        self.chores -= chore
-    
-    # def mark_as_done(self, chore, status_done):
+    def remove_chore(self, removal):
+        for chore in self.chores:
+            if chore == removal:
+                self.chores.remove(chore)
+
+    def mark_as_done(self, marked):
+        for chore in self.chores:
+            if marked == chore:
+                chore.status = "done"
         
     def display_list(self):
-        f"""
-            {self.chores}"""
-    
-    # def filter_list(self):
+        for chore in self.chores:
+            print(chore.title)   
 
+    def filter_list(self, status):
+        for chore in self.chores:
+            if status.lower() == chore.status:
+                print(chore)
+
+
+chore1 = Chore("Floors", "clean floors of the house", "ongoing")
+chore2 = Chore("Trash", "take rubish out", "ongoing")
+chore3 = Chore("Windows", "clean all windows", "ongoing")
+chore4 = Chore("Food shopping", "go grocery shopping", "ongoing")
+chore5 = Chore("Plan menus", "plan menus for the week", "ongoing")
+
+Todolist = List_of_chores()
+Todolist.add_chore(chore1)
+Todolist.add_chore(chore4)
+Todolist.add_chore(chore5)
+# Todolist.display_list()
+
+Todolist.remove_chore(chore1)
+# Todolist.display_list()
+Todolist.mark_as_done(chore4)
+# print(str(chore4))
+
+
+Todolist.display_list()
+
+# Todolist.filter_list("ongoing")
+Todolist.filter_list("DONE")
--- Day5/job1.py
+++ Day5/job1.py
@@ -1,3 +1,54 @@
+class Part:
+    def __init__(self, name, material):
+        self.name = name        
+        self.material = material
+
+    def change_material(self, new_material):
+        material = new_material
+
+    def __str__(self):
+        return f"""The {self.name} is made out of {self.material} """
+
 class Ship:
-    def __init__(self, name):
+    def __init__(self, name:str, part=Part):
         self.name = name 
+        self.__parts =  {"mast": Part("mast","wood"),                        
+                        "sail": Part("sail", "cloth"),
+                        "hull": Part("hull","wood"),
+                        "headsail": Part("headsail", "cloth"),
+                        "helm": Part("helm", "metal")}
+        
+    
+    # def display_state(self):
+    #     items = self.__parts.keys()
+    #     return f"The {self.name} is made of the following items: {items}"
+    
+    def display_state(self):
+        print(f"Ship's name : {self.name}")
+        for i in self.__parts.keys():
+            print(f"{i}")
+        
+
+    # def replace_part(self, part_name, new_part):
+    #     return part_name.pop(new_part)
+    
+
+mast = Part("mast","wood")
+hull = Part("hull", "wood")
+sail = Part("sail", "cloth")
+headsail = Part("headsail", "cloth")
+helm = Part("helm", "metal")
+mast = Part("mast", "metal")
+Titanic = Ship("Titanic", "mast")  
+
+# print(str(mast))
+# print(str(hull))
+# print(str(sail))
+# print(str(headsail))
+# print(str(helm))
+
+print(Titanic.display_state())
+# Titanic.replace_part("mast", "hull")
+
+
+        
